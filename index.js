51581bbf8eae14c4c0dd785479dcc5c909cf4c2b
seweek <yohikka@mail.ru>
2014-05-21 00:44:35 +0400

    fix

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ function saveImage(req, res) {
 		var parsdata = querystring.parse(data);
 		var newimage = parsdata.imagedata.split(',').pop();
 		var buffer = new Buffer(newimage, 'base64');
-		var usrnm = localStorage.username;
-		var photo = new Photo({user: usrnm});
+		var photo = new Photo({user: 'Anonymous'});
 		photo.save(function (err, result) {
 			if (err){
 				res.writeHead(302, {'Location': '/static/index.html'});
