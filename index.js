ba4247f49d7327acfb48f707fac6e3ef75cb3d8d
seweek <yohikka@mail.ru>
2014-05-21 00:27:47 +0400

    mew

--- index.js
+++ index.js
@@ -140,7 +140,7 @@ http.createServer(function(req, res) {
     }
 	if (url === '/') {
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(fs.readFileSync(__dirname + 'index.html'));
+        res.write(fs.readFileSync(__dirname + '/index.html'));
         res.end('\n');
 		return;
     }
