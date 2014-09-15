0e5c5af3cdc10eef5dcdf42ecb5f618c9384711e
seweek <yohikka@mail.ru>
2014-09-15 18:05:42 +0400

    trycatch
    sd

--- static/main.js
+++ static/main.js
@@ -13,14 +13,18 @@ window.onload = function(){
 	var rawimg;
 	document.getElementById("username").value = localStorage.username;
 	function applyGlitch(){
-		var ctx;
-		var ctx = canvas.getContext('2d');
-		ctx.putImageData(rawimg, 0,0);
-		var data = ctx.getImageData(0,0,canvas.width, canvas.height);
-		glitch(data, {amount: amountInput.value, seed: seedInput.value, iterations: iterationsInput.value, quality: qualityInput.value}, function(data){
-			ctx.putImageData(data, 0, 0);
-			imagedata.value = canvas.toDataURL('image/jpg');
-		})
+		try{
+			var ctx;
+			var ctx = canvas.getContext('2d');
+			ctx.putImageData(rawimg, 0,0);
+			var data = ctx.getImageData(0,0,canvas.width, canvas.height);
+			glitch(data, {amount: amountInput.value, seed: seedInput.value, iterations: iterationsInput.value, quality: qualityInput.value}, function(data){
+				ctx.putImageData(data, 0, 0);
+				imagedata.value = canvas.toDataURL('image/jpg');
+			})
+		} catch(e){
+		alert("error");
+		}
 	}
 	amountInput.onchange = function(){
 		applyGlitch();
